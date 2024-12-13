afd0618e674166998c0c35dba18bda6b71f774f2
Chitou <alaochitou8@gmail.com>
2024-12-13 13:32:07 +0000

    frontend fix for 404 requests

--- Design/src/services/api.js
+++ Design/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://a-event.onrender.com//api",
+  baseURL: "https://a-event.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
